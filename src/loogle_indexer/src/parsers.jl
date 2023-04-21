db9234b3ffc9597c211f113a6cb2d26a01787d51
brvtalcake <axlpascon@gmail.com>
2023-04-21 03:29:27 +0200

    Comment out processFilesInIndex: need to rewrite it

--- src/loogle_indexer/src/parsers.jl
+++ src/loogle_indexer/src/parsers.jl
@@ -95,81 +95,7 @@ function processFile(file::Dict{FileFields, Any}, chnl)::Tuple{Dict{FileFields, 
     return file, "", false
 end
 
-# All ParsingUnit in a ParserPool has the same number of files to parse than the others
-#= mutable struct ParsingUnit
-    to_parse::Vector{Tuple{Dict{FileFields, Any}, String, Bool}} # file, text, parsed
-    task_func::Vector{Tuple{Function, Task, Any, Bool}} # func, task, args, done
-    errors::Vector{String}
-    done::Bool
-    ParsingUnit() = error("You must specify at least one file to parse")
-    ParsingUnit(files::Vector{Dict{FileFields, Any}}) = new(zip(files, ["" for i=1:length(files)], [false for i=1:length(files)]),
-                                                            zip([processFile for i=1:length(files)],
-                                                                [Task(() -> func(arg)) for (func, arg) in zip([processFile for i=1:length(files)], files)],
-                                                                files,
-                                                                [false for i=1:length(files)]),
-                                                            Vector{String}(),
-                                                            false)
-    
-    ParsingUnit(file::Dict{FileFields, Any}) = new([(file, "", false)], [(processFile, Task(() -> processFile(file)), file, false)], Vector{String}(), false)
-
-    ParsingUnit(file::Dict{FileFields, Any}, func::Function) = new([(file, "", false)], [(func, Task(() -> func(file)), file, false)], Vector{String}(), false)
-
-    ParsingUnit(files::Vector{Dict{FileFields, Any}}, func::Function) = new(zip(files, ["" for i=1:length(files)], [false for i=1:length(files)]),
-                                                                            zip([func for i=1:length(files)],
-                                                                                [Task(() -> func_(arg)) for (func_, arg) in zip([func for i=1:length(files)], files)],
-                                                                                files,
-                                                                                [false for i=1:length(files)]),
-                                                                            Vector{String}(),
-                                                                            false)
-
-    function addFile(file::Dict{FileFields, Any})
-        push!(to_parse, (file, "", false))
-        push!(task_func, (processFile, Task(() -> processFile(file)), file, false))
-    end
-
-    function addFile(file::Dict{FileFields, Any}, func::Function)
-        push!(to_parse, (file, "", false))
-        push!(task_func, (func, Task(() -> func(file)), file, false))
-    end
-
-    function addFiles(files::Vector{Dict{FileFields, Any}})
-        for file in files
-            push!(to_parse, (file, "", false))
-            push!(task_func, (processFile, Task(() -> processFile(file)), file, false))
-        end
-    end
-
-    function addFiles(files::Vector{Dict{FileFields, Any}}, func::Function)
-        for file in files
-            push!(to_parse, (file, "", false))
-            push!(task_func, (func, Task(() -> func(file)), file, false))
-        end
-    end
-
-    function parse()
-        for (func, task, args, done) in task_func
-            if !done
-                schedule(task)
-            end
-        end
-        for (file, text, parsed) in to_parse
-            file[f_parsed] = true
-        end
-        for (func, task, args, done) in task_func
-            if !done
-                task_func[task_func .== (func, task, args, done)][4] = true
-            end
-        end
-        done = true
-    end
-
-end
-
-mutable struct ParserPool
-    
-end =#
-
-function processFilesInIndex(index::Index)::Vector{Typle{Dict{FileFields, Any}, String}}
+#= function processFilesInIndex(index::Index)::Vector{Typle{Dict{FileFields, Any}, String}}
     parsed_files::Vector{Tuple{Dict{FileFields, Any}, String}} = []
     chnl = Channel{Tuple{Dict{FileFields, Any}, String}, Bool}(Inf)
     files_per_task = (length(index.files) - (length(index.files) % Threads.nthreads())) / Threads.nthreads()
@@ -179,10 +105,6 @@ function processFilesInIndex(index::Index)::Vector{Typle{Dict{FileFields, Any}, 
         else
             files = index.files[(i-1)*files_per_task+1:i*files_per_task]
         end
-        #= Threads.@spawn for file in files
-            res = processFile(file, chnl)
-            push!(parsed_files, (res[1], res[2]))
-        end =#
         Threads.@spawn (function (files_to_be_parsed, parsed_files, chnl)
             for file in files_to_be_parsed
                 processFile(file, chnl)
@@ -198,4 +120,4 @@ function processFilesInIndex(index::Index)::Vector{Typle{Dict{FileFields, Any}, 
         end
     end
 
-end
+end =#
